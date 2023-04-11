aca124badbe01e96f2d008f71d422395d7a6a810
Paula Hitz <paulahitz8@gmail.com>
2023-04-11 11:58:56 +0200

    Event

---
+++ Assets/Scripts/Events/EventSystem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event
+{
+    public string id;
+    public bool done = false;
+}
+
+public class EventSystem : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
